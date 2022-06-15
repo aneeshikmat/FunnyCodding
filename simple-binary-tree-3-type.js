c00333497e5fb257e5ce4272d23c4706c59f1e94
Anees Hikmat Abu-hamid <aneeshikmat@gmail.com>
2022-06-15 15:34:51 +0300

    Create simple-binary-tree-3-type.js

---
+++ simple-binary-tree-3-type.js
@@ -0,0 +1,80 @@
+/*
+ Pre & in & post orders
+*/
+
+// Recursion
+
+
+// Iteration 
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {    
+    if(!root){
+        return [];
+    }
+    
+    let stack = [root];
+    let res = [];
+
+    while(stack.length > 0) {
+        let node = stack.pop();
+        
+        res.push(node.val);
+        
+        node.right && stack.push(node.right);
+        node.left && stack.push(node.left);
+    }
+    
+    return res;
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root) return [];
+    
+    let stack = [];
+    let res = [];
+    
+    while(root || stack.length > 0){
+        if(root){
+            stack.push(root);
+            root = root.left;
+        }else {
+            root = stack.pop();
+            res.push(root.val);
+            root = root.right;
+        }
+    }
+    
+    return res;
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {// O(n^2)
+    if(!root){
+        return [];
+    }
+    
+    let stack = [root];
+    let res = [];
+
+    while(stack.length > 0) {
+        let node = stack.pop();
+        
+        res.unshift(node.val);
+        
+        node.left && stack.push(node.left);
+        node.right && stack.push(node.right);
+    }
+    
+    return res;
+};
